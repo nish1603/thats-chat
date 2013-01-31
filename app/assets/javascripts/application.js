247475b7c0278f3c87c844537b6114d9f7b8c07a
Nishant Tuteja <nishant.tuteja@vinsol.com>
2013-02-01 00:44:35 +0530

    removed unused js in application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,21 +14,3 @@
 //= require jquery_ujs
 //= require jquery-ui
 //= require_tree .
-
- 
-        function debug(str){ $("#debug").append("<p>"+str+"</p>"); };
-
-        ws = new WebSocket('ws://127.0.0.1:3000');
-        ws.onmessage = function(evt) { $("#msg").append("<p>"+evt.data+"</p>"); };
-        ws.onclose = function() { debug("socket closed"); };
-        ws.onopen = function() {
-          debug("connected...");
-          alert("hello");
-          ws.send("hello server");
-        };
-
-        $('form#new_message').live('submit', function(){
-          console.log('baby');
-          ws.send('baby');
-        });
- 
